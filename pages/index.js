5945860a887259814d790e96a3caf71ccaf61287
pauljsyi <pauljsyi@gmail.com>
2022-02-06 21:26:03 -0800

    created modal and product page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import React from "react";
 
-const Index = () => (
-  <Page
-    title="Products"
-    primaryAction={{
-      content: "Select Products",
-      onAction: () => console.log("clicked"),
-    }}
-  />
-);
+class Index extends React.Component {
+  state = { open: false };
+  render() {
+    return (
+      <Page
+        title="Products"
+        primaryAction={{
+          content: "Select Products",
+          onAction: () => this.setState({ open: true }),
+        }}
+      >
+        <ResourcePicker
+          resourceType="Product"
+          // showVariant={false}
+          open={this.state.open}
+        />
+      </Page>
+    );
+  }
+}
 
 export default Index;
